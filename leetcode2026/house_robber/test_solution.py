90e85a4a2c76730740ca165d296197c953840b92
yangyong01 <yangyong01@corp.netease.com>
2026-03-05 11:23:43 +0800

    refactor(tests): split table-driven tests into individual test methods

--- leetcode2026/house_robber/test_solution.py
+++ leetcode2026/house_robber/test_solution.py
@@ -7,16 +7,26 @@ class TestSolution(TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test_rob(self):
-        test_cases = [
-            ([1, 2, 3, 1], 4),
-            ([2, 7, 9, 3, 1], 12),
-            ([5], 5),
-            ([2, 1], 2),
-            ([3, 3, 3, 3], 6),
-            ([1, 2, 3, 4, 5], 9),
-            ([0, 0, 0], 0),
-            ([], 0),
-        ]
-        for nums, expected in test_cases:
-            self.assertEqual(self.solution.rob(nums), expected)
+    def test_two_non_adjacent(self):
+        self.assertEqual(self.solution.rob([1, 2, 3, 1]), 4)
+
+    def test_longer_sequence(self):
+        self.assertEqual(self.solution.rob([2, 7, 9, 3, 1]), 12)
+
+    def test_single_element(self):
+        self.assertEqual(self.solution.rob([5]), 5)
+
+    def test_two_elements(self):
+        self.assertEqual(self.solution.rob([2, 1]), 2)
+
+    def test_all_same(self):
+        self.assertEqual(self.solution.rob([3, 3, 3, 3]), 6)
+
+    def test_increasing(self):
+        self.assertEqual(self.solution.rob([1, 2, 3, 4, 5]), 9)
+
+    def test_all_zeros(self):
+        self.assertEqual(self.solution.rob([0, 0, 0]), 0)
+
+    def test_empty(self):
+        self.assertEqual(self.solution.rob([]), 0)
--- leetcode2026/letter_combinations_of_a_phone_number/test_solution.py
+++ leetcode2026/letter_combinations_of_a_phone_number/test_solution.py
@@ -7,73 +7,82 @@ class TestSolution(TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test_letterCombinations(self):
-        test_cases = [
-            # 题目样例
-            ("23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]),
-            # 空输入
-            ("", []),
-            # 单数字（3字母键）
-            ("2", ["a", "b", "c"]),
-            # 单数字（4字母键）
-            ("9", ["w", "x", "y", "z"]),
-            # 两个4字母键
-            (
-                "79",
-                [
-                    "pw",
-                    "px",
-                    "py",
-                    "pz",
-                    "qw",
-                    "qx",
-                    "qy",
-                    "qz",
-                    "rw",
-                    "rx",
-                    "ry",
-                    "rz",
-                    "sw",
-                    "sx",
-                    "sy",
-                    "sz",
-                ],
-            ),
-            # 三位数字（3*3*3=27种）
-            (
-                "234",
-                [
-                    "adg",
-                    "adh",
-                    "adi",
-                    "aeg",
-                    "aeh",
-                    "aei",
-                    "afg",
-                    "afh",
-                    "afi",
-                    "bdg",
-                    "bdh",
-                    "bdi",
-                    "beg",
-                    "beh",
-                    "bei",
-                    "bfg",
-                    "bfh",
-                    "bfi",
-                    "cdg",
-                    "cdh",
-                    "cdi",
-                    "ceg",
-                    "ceh",
-                    "cei",
-                    "cfg",
-                    "cfh",
-                    "cfi",
-                ],
-            ),
-            # 相同数字重复
-            ("22", ["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"]),
-        ]
-        for digits, expected in test_cases:
-            self.assertCountEqual(self.solution.letterCombinations(digits), expected)
+    def test_example(self):
+        self.assertCountEqual(
+            self.solution.letterCombinations("23"),
+            ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"],
+        )
+
+    def test_empty(self):
+        self.assertCountEqual(self.solution.letterCombinations(""), [])
+
+    def test_single_digit_three_letters(self):
+        self.assertCountEqual(self.solution.letterCombinations("2"), ["a", "b", "c"])
+
+    def test_single_digit_four_letters(self):
+        self.assertCountEqual(
+            self.solution.letterCombinations("9"), ["w", "x", "y", "z"]
+        )
+
+    def test_two_four_letter_keys(self):
+        self.assertCountEqual(
+            self.solution.letterCombinations("79"),
+            [
+                "pw",
+                "px",
+                "py",
+                "pz",
+                "qw",
+                "qx",
+                "qy",
+                "qz",
+                "rw",
+                "rx",
+                "ry",
+                "rz",
+                "sw",
+                "sx",
+                "sy",
+                "sz",
+            ],
+        )
+
+    def test_three_digits(self):
+        self.assertCountEqual(
+            self.solution.letterCombinations("234"),
+            [
+                "adg",
+                "adh",
+                "adi",
+                "aeg",
+                "aeh",
+                "aei",
+                "afg",
+                "afh",
+                "afi",
+                "bdg",
+                "bdh",
+                "bdi",
+                "beg",
+                "beh",
+                "bei",
+                "bfg",
+                "bfh",
+                "bfi",
+                "cdg",
+                "cdh",
+                "cdi",
+                "ceg",
+                "ceh",
+                "cei",
+                "cfg",
+                "cfh",
+                "cfi",
+            ],
+        )
+
+    def test_repeated_digit(self):
+        self.assertCountEqual(
+            self.solution.letterCombinations("22"),
+            ["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"],
+        )
